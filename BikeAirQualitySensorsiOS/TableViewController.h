21c80b8525f6cdfb80c01f060ba7dbda0c00ab46
sighmon <simon.loffler@gmail.com>
2015-10-05 14:11:53 +1030

    Initial commit. Based on RedBear iOS BLE serial example.

---
+++ BikeAirQualitySensorsiOS/TableViewController.h
@@ -0,0 +1,22 @@
+//
+//  TableViewController.h
+//  SimpleControl
+//
+//  Created by Cheong on 7/11/12.
+//  Copyright (c) 2012 RedBearLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BLE.h"
+
+@interface TableViewController : UITableViewController <BLEDelegate>
+{
+    IBOutlet UIButton *btnConnect;
+    IBOutlet UIActivityIndicatorView *indConnecting;
+    IBOutlet UILabel *lblRSSI;
+    IBOutlet UILabel *sensorValues;
+}
+
+@property (strong, nonatomic) BLE *ble;
+
+@end
---
+++ BikeAirQualitySensorsiOS/TableViewController.m
@@ -0,0 +1,130 @@
+//
+//  TableViewController.m
+//  SimpleControl
+//
+//  Created by Cheong on 7/11/12.
+//  Copyright (c) 2012 RedBearLab. All rights reserved.
+//
+
+#import "TableViewController.h"
+
+@interface TableViewController ()
+
+@end
+
+@implementation TableViewController
+
+@synthesize ble;
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    ble = [[BLE alloc] init];
+    [ble controlSetup];
+    ble.delegate = self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - BLE delegate
+
+NSTimer *rssiTimer;
+
+- (void)bleDidDisconnect
+{
+    NSLog(@"->Disconnected");
+
+    [btnConnect setTitle:@"Connect" forState:UIControlStateNormal];
+    [indConnecting stopAnimating];
+    
+    lblRSSI.text = @"---";
+    sensorValues.text = @"t: --- h: --- s: --- *---*";
+    
+    [rssiTimer invalidate];
+}
+
+// When RSSI is changed, this will be called
+-(void) bleDidUpdateRSSI:(NSNumber *) rssi
+{
+    lblRSSI.text = rssi.stringValue;
+}
+
+-(void) readRSSITimer:(NSTimer *)timer
+{
+    [ble readRSSI];
+}
+
+// When disconnected, this will be called
+-(void) bleDidConnect
+{
+    NSLog(@"->Connected");
+
+    [indConnecting stopAnimating];
+
+    // Schedule to read RSSI every 1 sec.
+    rssiTimer = [NSTimer scheduledTimerWithTimeInterval:(float)1.0 target:self selector:@selector(readRSSITimer:) userInfo:nil repeats:YES];
+}
+
+// When data is comming, this will be called
+-(void) bleDidReceiveData:(unsigned char *)data length:(int)length
+{
+    NSLog(@"Length: %d", length);
+    NSLog(@"Data: %s", data);
+    sensorValues.text = [NSString stringWithFormat:@"%s", data];
+}
+
+#pragma mark - Actions
+
+// Connect button will call to this
+- (IBAction)btnScanForPeripherals:(id)sender
+{
+    if (ble.activePeripheral)
+        if(ble.activePeripheral.state == CBPeripheralStateConnected)
+        {
+            [[ble CM] cancelPeripheralConnection:[ble activePeripheral]];
+            [btnConnect setTitle:@"Connect" forState:UIControlStateNormal];
+            return;
+        }
+    
+    if (ble.peripherals)
+        ble.peripherals = nil;
+    
+    [btnConnect setEnabled:false];
+    [ble findBLEPeripherals:2];
+    
+    [NSTimer scheduledTimerWithTimeInterval:(float)2.0 target:self selector:@selector(connectionTimer:) userInfo:nil repeats:NO];
+    
+    [indConnecting startAnimating];
+}
+
+-(void) connectionTimer:(NSTimer *)timer
+{
+    [btnConnect setEnabled:true];
+    [btnConnect setTitle:@"Disconnect" forState:UIControlStateNormal];
+    
+    if (ble.peripherals.count > 0)
+    {
+        [ble connectPeripheral:[ble.peripherals objectAtIndex:0]];
+    }
+    else
+    {
+        [btnConnect setTitle:@"Connect" forState:UIControlStateNormal];
+        [indConnecting stopAnimating];
+    }
+}
+
+@end
